a1e3a4d3d6e50820426eb56baca19c714650c2ef
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2023-03-23 17:57:29 -0300

    add

---
+++ tipos/tipos.js
@@ -0,0 +1,16 @@
+"use strict";
+//string
+let nome = 'Augusto';
+console.log(nome);
+//nome
+let idade = 27;
+idade = 27.5;
+console.log(idade);
+//boolean 
+let possuiHobbies = true;
+if (possuiHobbies) {
+    console.log('possuihobbies true');
+}
+else {
+    console.log('possuihobbies false');
+}
---
+++ tipos/tipos.ts
@@ -0,0 +1,20 @@
+//string
+let nome = 'Augusto';
+console.log(nome)
+
+//nome
+
+let idade = 27
+idade = 27.5
+console.log(idade)
+
+//boolean 
+let possuiHobbies = true
+
+if(possuiHobbies){
+console.log('possuihobbies true')
+}else {
+    console.log('possuihobbies false')
+
+}
+
