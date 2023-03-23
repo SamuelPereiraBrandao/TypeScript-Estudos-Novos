6697c180238a26c1f31b57271d7333f6c0e14679
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2023-03-23 11:03:37 -0300

    add

---
+++ introducao/basico.js
@@ -0,0 +1,20 @@
+"use strict";
+const searchInput = document.querySelector("#search-input");
+searchInput.addEventListener("input", () => {
+    const searchTerm = searchInput.value;
+    console.log(searchTerm); // Exemplo: mostra o termo de pesquisa no console
+});
+const itemList = document.querySelectorAll("ul li");
+searchInput.addEventListener("input", () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    itemList.forEach(item => {
+        var _a, _b;
+        const text = (_b = (_a = item.textContent) === null || _a === void 0 ? void 0 : _a.toLowerCase()) !== null && _b !== void 0 ? _b : "";
+        if (text.includes(searchTerm)) {
+            item.classList.remove("hidden");
+        }
+        else {
+            item.classList.add("hidden");
+        }
+    });
+});
--- introducao/basico.ts
+++ introducao/basico.ts
@@ -1,2 +1,23 @@
-const a: string = 'Teste TS!'
-console.log(a)
+const searchInput = document.querySelector("#search-input") as HTMLInputElement;
+
+searchInput.addEventListener("input", () => {
+  const searchTerm = searchInput.value;
+  console.log(searchTerm); // Exemplo: mostra o termo de pesquisa no console
+});
+
+
+const itemList = document.querySelectorAll("ul li");
+
+searchInput.addEventListener("input", () => {
+  const searchTerm = searchInput.value.toLowerCase();
+
+  itemList.forEach(item => {
+    const text = item.textContent?.toLowerCase() ?? "";
+
+    if (text.includes(searchTerm)) {
+      item.classList.remove("hidden");
+    } else {
+      item.classList.add("hidden");
+    }
+  });
+});
